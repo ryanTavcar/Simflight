f33782914ef6e8e2f77e3d4bd8c6f8336d144f6d
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2021-08-29 14:25:14 +1000

    filter products

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
@@ -6,20 +6,48 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Link from 'next/link'
 import ProductCard from '../../components/ProductCard';
-import products from '../../util/products';
+import products, {elite, igate} from '../../util/products';
 
 const Products = () => {
+
+    const [prods, setProds] = useState([])
+    const handleProducts = (e) => {
+        console.log(e.target.innerText)
+        switch (e.target.innerText) {
+            case 'ELITE':
+                return setProds(elite)
+            case 'IGATE':
+                return setProds(igate)
+        }
+    }
+
+    useEffect(() => {
+        setProds(elite)
+    }, [])
+
     return (
         <Container maxWidth="xl">
             <Grid container>
                 <Grid item xs={12} md={12}>
 
                 <Box my={4}>
-                    <Typography variant="h4" component="h1" gutterBottom>
-                        Product page
+                    <Typography variant="h4" component="h1" >
+                        View by:
                     </Typography>
                 </Box>
-                    {products.map(product => (
+                <Box>
+                    <Button variant="contained" color="default" onClick={(e) => handleProducts(e)}>
+                        <Typography variant="h5" component="h5" gutterBottom>
+                            Elite
+                        </Typography>
+                    </Button>
+                    <Button variant="contained" color="default" onClick={(e) => handleProducts(e)}>
+                        <Typography variant="h5" component="h5" gutterBottom>
+                            iGate
+                        </Typography>
+                    </Button>
+                </Box>
+                    {prods.map(product => (
                         <Box my={4} key={product.name}>
                             <ProductCard product={product}/>
                         </Box>
